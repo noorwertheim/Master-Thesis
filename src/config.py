9226ae7a944592e606a992360e800f99c5a1d4c0
noorwertheim <noorwertheim@gmail.com>
2025-03-12 09:43:57 +0100

    changed directory of model save

--- src/config.py
+++ src/config.py
@@ -3,3 +3,4 @@ import os
 raw_data_path = os.path.join('..', 'data', 'raw')
 processed_data_path = os.path.join('..', 'data', 'processed')
 univariate_data_path = os.path.join('..', 'data', 'univariate')
+models_path = os.path.join('..', 'models')
