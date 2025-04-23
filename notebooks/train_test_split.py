d90ab86a21bf23e143b52478ee93d445a355029f
noorwertheim <noorwertheim@gmail.com>
2025-04-23 13:50:47 +0200

    train test split and ran all target data again

--- notebooks/train_test_split.py
+++
@@ -1,9 +0,0 @@
-import os
-import numpy as np
-from config import raw_data_path, univariate_data_path, processed_data_path, models_path
-
-save_dir = os.path.join(univariate_data_path, "target_univariate.npy")
-target_data = np.load(save_dir, allow_pickle=True)
-target_data = [item for item in target_data if item['preterm'] is not None]
-
-
